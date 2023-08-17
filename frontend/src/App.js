681e959131775a6090f1b0caf101612eb3798b47
João Alcaraz <joao.alcaraz@sou.inteli.edu.br>
2023-08-17 15:20:16 -0300

    as

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,54 +6,43 @@ function App() {
 
   const [data, setData] = useState([]);
 
-  const reqData = () => { 
-    fetch('http://10.0.132.252:8000/users')
-    .then(response => response.json())
-    .then(data => {
-      console.log('Response data:', data);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-  }
-
-  reqData();
-  // useEffect(() => {
-  //   fetch('http://10.0.132.252:8000/users') // Add 'http://' before the IP address
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  //     .then(data => setData(data))
-  //     .catch(error => console.error('Error fetching data:', error));
-  // }, []);
+  useEffect(() => {
+    fetch('http://10.0.132.252:8000/users') // Add 'http://' before the IP address
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
   
-  // let users = data.map((user) => {
-  //   return <User key={user.id} name={user.name} age={user.age} date={user.data} />
-  // });
+  let users = data.map((user) => {
+    return <User key={user.id} name={user.name} age={user.age} date={user.data} />
+  });
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const name = e.target.name.value;
-  //   const age = e.target.age.value;
-  //   const newUser = {name, age};
-  //   console.log(newUser)
-  //   fetch('http://10.0.132.252:8000/users', {
-  //     method: 'POST',
-  //     body: JSON.stringify(newUser),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then(response => response.json())
-  //     .then(setData([...data, newUser]));
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const age = e.target.age.value;
+    const newUser = {name, age};
+    console.log(newUser)
+    fetch('http://10.0.132.252:8000/users', {
+      method: 'POST',
+      body: JSON.stringify(newUser),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then(setData([...data, newUser]));
+  }
 
   return (
     <>
       <div className="App" id = "users">
         <h2>Users</h2>
+        {users}
       </div>
       <div>
         <h2>Add User</h2>
-        {/* <form onSubmit={handleSubmit}> */}
+        <form onSubmit={handleSubmit}>
         <fieldset>
          <label>
            <p>Name</p>
@@ -65,7 +54,7 @@ function App() {
          </label>
        </fieldset>
        <button type="submit">Submit</button>
-        {/* </form> */}
+        </form>
       </div>
     </>
   );
