0502c1b33ee96477a266b9a660b29b3599801697
João Alcaraz <joao.alcaraz@sou.inteli.edu.br>
2023-08-18 11:33:06 -0300

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
 
   const teste = async () => {
     console.log('teste');
-    const url = '/users';
+    const url = 'http://10.0.132.252:8000/users';
     console.log(url);
     const response = await fetch(url);
     console.log("passou");
