0d4e4c42de6542b40d01a62fb5ea950bc7186d60
João Alcaraz <joao.alcaraz@sou.inteli.edu.br>
2023-08-18 12:08:40 -0300

    ax

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/users',
+    createProxyMiddleware({
+      target: 'http://10.0.132.252:8000',
+      changeOrigin: true,
+    })
+  );
+};
